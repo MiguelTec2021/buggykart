f836b8b34b0ad9625a6841175795406f7f77c006
Miguel Ortiz <tecnicohernandez2018@gmail.com>
2021-11-06 12:25:23 -0600

    Pagina StartPage

--- buggykart/lib/src/pages/start_page.dart
+++ buggykart/lib/src/pages/start_page.dart
@@ -9,8 +9,47 @@ class StartPage extends StatefulWidget {
 }
 
 class _StartPageState extends State<StartPage> {
+  final TextStyle _textTitle = const TextStyle(fontSize: 40.0);
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Comunidad', style: _textTitle,),
+            Text('Buggy Kart',style: _textTitle,),
+            const SizedBox(height: 40,),
+            Container(
+              height: 100.0,
+              padding: const EdgeInsets.all(20.0),
+              child: const FadeInImage(
+                placeholder: AssetImage('assets/logo_j_m.png'),
+                image: AssetImage('assets/logo_j_m.png')),
+            ),
+            const SizedBox(height: 50.0,),
+            Text('Bienvenido', style: _textTitle,),
+            const SizedBox(height: 50.0,),
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 20.0),
+              child: const Text('Comunidad del juego Buggy Kart donde puede compartir su experiencia', textAlign: TextAlign.center, ),
+            ),
+            const SizedBox(height: 60.0,),
+            ElevatedButton(
+              onPressed: (){
+                Navigator.push(context, MaterialPageRoute(builder: (context)=>StartPage()));
+              },
+              child: const Text("Empezar",style: TextStyle(color: Colors.white, fontSize: 20.0)),
+              style: ElevatedButton.styleFrom(
+                primary: Colors.red,
+                onPrimary: Colors.blue
+              )
+              
+              )
+
+          ],
+        ),
+      ),
+    );
   }
 }
