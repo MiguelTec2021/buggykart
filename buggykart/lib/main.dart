6a8d14e07b5faca48762a907995ea0f1f9381b4d
Miguel Ortiz <tecnicohernandez2018@gmail.com>
2021-11-22 17:47:12 -0600

    Eliminando lineas de comentarios de codigo

--- buggykart/lib/main.dart
+++ buggykart/lib/main.dart
@@ -1,5 +1,3 @@
-import 'package:buggykart/src/pages/add_new.dart';
-// import 'package:buggykart/src/pages/home_page.dart';
 import 'package:buggykart/src/pages/login_page.dart';
 import 'package:buggykart/src/pages/register_user_page.dart';
 import 'package:buggykart/src/pages/start_page.dart';
@@ -18,13 +16,10 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.teal,
       ),
-      // home: StartPage(),
       initialRoute: '/',
       routes: {
         '/'        : (context) => const StartPage(),
         '/login'   : (context) => const LoginPage(),
-        // '/home'    : (context) =>const HomePage(),
-        // '/addnew'  : (context) =>  const AddNew(),
         '/register': (context) => const RegisterUserPage(),
 
       },
